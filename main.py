5f822a363410302d5f2a851ad74c9e33deca38c7
Xavier <xavierdamours@gmail.com>
2021-11-29 18:56:25 -0500

    Added convolution & pooling

--- main.py
+++ main.py
@@ -15,17 +15,21 @@ print(test_images.shape)
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(10)
-])
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Conv2D(32, (10, 10), padding='same', activation='tanh', input_shape=(28, 28, 1)))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(3, 3)))
+model.add(tf.keras.layers.Conv2D(64, (4, 4), padding='same', activation='tanh', input_shape=(28, 28, 1)))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(3, 3)))
+model.add(tf.keras.layers.Flatten(input_shape=(28,28,1)))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(10))
+model.summary()
 
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=10)
+model.fit(train_images, train_labels, epochs=5, batch_size=64)
 
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 
